f36fe280f7a8d12c4e11dba2a494ce4ca8b0035f
Dragondagi <149649489+Dragondagi@users.noreply.github.com>
2026-05-07 22:10:35 +0300

    --

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   cloudflare: false,
   tanstackStart: {
-    nitro: {
+    server: {
       preset: "vercel",
     },
   },
